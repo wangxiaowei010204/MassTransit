b9afab572ce7e2950fee71b9ce343c0d4c7f7fbc
wangxiaowei010204 <844503316@qq.com>
2020-04-04 10:00:52 +0800

    提交

---
+++ Receiver/Program.cs
@@ -0,0 +1,71 @@
+﻿using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "MassTransit Server";
+
+            var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri("rabbitmq://127.0.0.1/EDCVHOST"), hst =>
+                {
+                    hst.Username("admin");
+                    hst.Password("edison");
+                });
+
+                cfg.ReceiveEndpoint(host, "Qka.MassTransitTest", e =>
+                {
+                    e.Consumer<TestConsumerClient>();
+                    e.Consumer<TestConsumerAgent>();
+                });
+            });
+
+            bus.Start();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+            bus.Stop();
+        }
+    }
+
+    public class TestConsumerClient : IConsumer<Client>
+    {
+        public async Task Consume(ConsumeContext<Client> context)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            await Console.Out.WriteLineAsync($"Receive message: {context.Message.Id}, {context.Message.Name}, {context.Message.Birthdate.ToString()}");
+            Console.ResetColor();
+        }
+    }
+
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class TestConsumerAgent : IConsumer<Agent>
+    {
+        public async Task Consume(ConsumeContext<Agent> context)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            await Console.Out.WriteLineAsync($"Receive message: {context.Message.AgentCode}, {context.Message.AgentName}, {context.Message.AgentRole}");
+            Console.ResetColor();
+        }
+    }
+
+    public class Agent
+    {
+        public int AgentCode { get; set; }
+        public string AgentName { get; set; }
+        public string AgentRole { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,65 @@
+﻿using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #if DEBUG
+                  Console.WriteLine("DEBUG:11111111111");
+            #else
+                  Console.WriteLine("Release:222222222222");
+            #endif
+
+            Console.Title = "MassTransit Client";
+
+            var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri("rabbitmq://127.0.0.1/EDCVHOST"), hst =>
+                {
+                    hst.Username("admin");
+                    hst.Password("edison");
+                });
+            });
+
+            var uri = new Uri("rabbitmq://127.0.0.1/EDCVHOST/Qka.MassTransitTest");
+            var message = Console.ReadLine();
+
+            while (message != null)
+            {
+                Task.Run(() => SendCommand(bus, uri, message)).Wait();
+                message = Console.ReadLine();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static async void SendCommand(IBusControl bus, Uri sendToUri, string message)
+        {
+            var endPoint = await bus.GetSendEndpoint(sendToUri);
+            var command = new Client()
+            {
+                Id = 100001,
+                Name = "Edison Zhou",
+                Birthdate = DateTime.Now.AddYears(-18),
+                Message = message
+            };
+
+            await endPoint.Send(command);
+
+            Console.WriteLine($"You Sended : Id = {command.Id}, Name = {command.Name}, Message = {command.Message}");
+        }
+    }
+
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Message { get; set; }
+    }
+
+}
